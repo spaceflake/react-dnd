d1e3b535c3fe578192bdd8a188702af2fb5572f7
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-05-28 10:44:56 +0200

    some styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Dropzone from './Dropzone';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1 style={{ color: '#eee', textShadow: '1px 1px 2px #aaa' }}>
+        Simple Drag and Drop
+      </h1>
       <Dropzone />
     </div>
   );
